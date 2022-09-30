dfaddba772aa0a626a96c758dd1de1d808864203
HodaGamal <77369927+Hoda233@users.noreply.github.com>
2022-09-30 20:43:35 +0200

    Add files via upload

---
+++ src/pages/TeamsFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>Teams Footer</h2>
+  </template>
+
+<style scoped>
+    h2 {
+      width: 100%;
+      height: 5rem;
+      padding: 2rem;
+      background-color: #11005c;
+      text-align: center;
+      color: white;
+      justify-content: center;
+      align-items: center;
+    }
+</style>
